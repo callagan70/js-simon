8e4738ef3bbe7a9f525118b2fa6573b2ff403e0d
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-23 16:54:27 +0000

    Done ! e ripulito

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var simon = []
-// var risposta =[]
 
     for (let i = 1; i < 6; i++) {
             x = Math.floor(Math.random() * 4 +1)
@@ -8,16 +7,15 @@ var simon = []
                 simon.push(x)
     }
 
-
 setTimeout (myFunction, 2000);
 
 console.log(simon)
 
 function myFunction(){
-for (let i = 0; i < 5; i++) {
-    y = prompt ("Caccia il " + i + "° numero")
-    if (simon[i] != y){
-        document.getElementById("risultato").innerHTML = "Imbecille ! Manco cinque numeri riesci a memorizzare !"
+    for (let i = 0; i < 5; i++) {
+        y = prompt (`Caccia il ${i}° numero`)
+        if (simon[i] != y){
+            document.getElementById("risultato").innerHTML = "Imbecille ! Manco cinque numeri riesci a memorizzare !"
         }
         else {
             document.getElementById("risultato").innerHTML = "Bravo hai vinto"
